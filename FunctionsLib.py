a6567b58dcff101c29b9c305cb0338bbbd3e5b34
Clara Rossignol <clararossignol@hotmail.com>
2021-07-27 18:17:21 +0200

    continuing clustering

--- FunctionsLib.py
+++ FunctionsLib.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import random
 import deeplabcut
+import math
 from IPython.display import Image, display
 
 
@@ -14,8 +15,8 @@ def corruption_data(original,scorer,bodyparts,list_i,n):
 
     for name in corrupted_frames:
         for i in list_i:
-            df.loc[name][(scorer,bodyparts[i], 'x')] = np.random.uniform(5,500)
-            df.loc[name][(scorer,bodyparts[i], 'y')] = np.random.uniform(5,500)
+            df.loc[name][(scorer,bodyparts[i], 'x')] = np.random.uniform(5,200)
+            df.loc[name][(scorer,bodyparts[i], 'y')] = np.random.uniform(5,100)
             
     return df,corrupted_frames
 
@@ -42,7 +43,24 @@ def swap_labels(original,scorer,bodyparts,i,j,n):
     return df,swapped_frames
 
 
-# calculate the angle between 3 points
+# calculate the distance between 2 keypoints
+def distance_keypoints(df,kpt1,kpt2):
+    bdpt1 = df.xs(kpt1, level='bodyparts', axis=1).to_numpy()
+    bdpt2 = df.xs(kpt2, level='bodyparts', axis=1).to_numpy()
+    diff = (bdpt1-bdpt2).reshape(len(df), -1, 2)
+    dist = np.linalg.norm(diff, axis=2)
+    return dist
+
+
+# calculate the angle between 3 keypoints (if negative, swapping might have occured)
 def getAngle(a,b,c):
     angle = math.degrees(math.atan2(c[1]-b[1],c[0]-b[0]) - math.atan2(a[1]-b[1],a[0]-b[0]))
     return angle
+
+
+# remove any NaN values from our dataset
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep].astype(np.float64)
---
+++ test_functions.py
@@ -0,0 +1,16 @@
+import unittest
+
+class MyTest(unittest.TestCase):
+    def test_swap():
+        #blabla
+    
+    def test_getAngle():
+        #blabla
+        
+        
+    def test_clean_dataset():
+        
+        
+        
+        
+        
